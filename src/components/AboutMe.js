e53a7432a93ebc82fad01e2b1b544b38c80fe886
lauraherrero <lauraherrero36@gmail.com>
2020-03-14 15:34:42 +0100

    Add animation in the hero

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import myPhoto from "../images/myPhoto.jpeg";
+import CV from "../images/CV_LauraHerrero.pdf";
 
 const AboutMe = () => {
     return (
@@ -13,6 +14,7 @@ const AboutMe = () => {
                 <p className="aboutme__content--text">Tras liderar los equipos de atención al cliente de Telepizza, fui consciente de la importancia de valorar y cuidar la marca de una empresa y su producto. Mi curiosidad me llevó a querer aprender más y descubrí la programación a través de Codecademy.</p>
                 <p className="aboutme__content--text"> Más adelante, decidí formarme como programadora front-end en Adalab, especializándome en JavaScript (ES6) y React, aplicando metodologías ágiles dentro del marco de trabajo Scrum.</p> 
                 <p className="aboutme__content--text">El trabajo en equipo es uno de mis puntos fuertes, reforzado por la organización, adaptabilidad y comunicación.</p>
+                <a href={CV} target="_blank" rel="noopener noreferrer" className="aboutme__content--link"><span className="aboutme__content--cv">Ver CV</span></a>
             </div>
         </div>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero__wrapper">
-                <h1 className="hero__title">Laura Herrero</h1>
+                <h1 className="hero__title typewriter">Laura Herrero</h1>
                 <div className="hero__bar" />
                 <h2 className="hero__position">Front-end developer</h2>
             </div>
